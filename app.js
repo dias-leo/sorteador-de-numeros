258ac7bd5c33b6fc1b7a710c8c84477e84365749
Leonardo Dias <leonardodiasdasilva1990@gmail.com>
2024-07-16 03:08:13 -0300

    teste inicial de variáveis do sorteador

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`quantidade: ${quantidade}`)
+    alert(`do número: ${de}`)
+    alert(`até o número: ${ate}`)
+}
